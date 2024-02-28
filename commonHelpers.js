c4b9588fb99fd1825c971f343e4dc51c2f0a1b7c
laposhko <92787511+laposhko@users.noreply.github.com>
2024-02-28 11:18:04 +0000

    Deploying to gh-pages from @ laposhko/goit-js-hw-11@3df1e206995a9d49c8bb936521c86e5958f15449 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,18 @@
-import{S as f}from"./assets/vendor-da53b1cb.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&c(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const d=document.querySelector(".images-list");function m(r){const o=r.map(n=>`<li class="gallery-item">
-    <a class="gallery-link" href="${n.pageURL}">
+import{S as m,i as p}from"./assets/vendor-9e112560.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=t(e);fetch(e.href,r)}})();const a=document.querySelector(".images-list");function y(o){a.innerHTML="";const s=o.map(t=>`<li class="gallery-item">
+    <a class="gallery-link" href="${t.webformatURL}">
     <img
       class="gallery-image"
-      src="${n.previewURL}"
-      // data-source="${n.largeURL}"
-      alt="${n.tags}"
+      src="${t.previewURL}"
+      // data-source="${t.largeImageURL}"
+      alt="${t.tags}"
     />
+      <ul class='statistic'>
+      <li>Likes:<br>${t.likes}</li>
+      <li>Views:<br>${t.views}</li>
+      <li>Comments:<br>${t.comments}</li>
+      <li>Downloads:<br>${t.downloads}</li>
+    </ul>
   </a>
-</li>`).join("");d.insertAdjacentHTML("beforeend",o),new f(".images-list li",{overlayOpacity:.8,captionsData:"alt",captionDelay:250})}const y="https://pixabay.com/api/",p="42495238-f221500a84206b905dda8310d";let i,l;function g(r){i=`${y}?key=${p}&q=${r}`,console.log(i),h().then(o=>{console.log(o),o.total==0&&document.querySelector(".content").insertAdjacentHTML("beforeend","<p>Sorry, there are no images matching your search query. Please try again!</p>"),l=o.hits.slice(0,9),m(l)})}function h(){return fetch(i).then(r=>{if(!r.ok)throw new Error("Image error!");return r.json()}).catch(r=>{alert("Error")})}document.querySelector(".submit-button");const u=document.querySelector(".search-form"),L=u.querySelector("input");document.querySelector(".content");let a;u.addEventListener("submit",r=>{r.preventDefault(),a=L.value,g(a)});
+
+</li>`).join("");a.insertAdjacentHTML("beforeend",s),new m(".images-list a",{overlayOpacity:.8})}const g="https://pixabay.com/api/",h="42495238-f221500a84206b905dda8310d";let d,l;function L(o){d=`${g}?key=${h}&q=${o}`,b().then(s=>{s.total==0&&p.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",backgroundColor:"#EF4040",theme:"dark",position:"topRight",maxWidth:"400px"}),l=s.hits.slice(0,9),y(l),document.querySelector(".loader").remove()})}function b(){return fetch(d).then(o=>{if(!o.ok)throw new Error("Image error!");return o.json()}).catch(o=>{alert("Error")})}const f=document.querySelector(".search-form"),c=f.querySelector("input"),$=document.querySelector(".content");let u;f.addEventListener("submit",o=>{o.preventDefault(),u=c.value,$.insertAdjacentHTML("afterbegin",'<span class="loader"></span>'),L(u),c.value=""});
 //# sourceMappingURL=commonHelpers.js.map
